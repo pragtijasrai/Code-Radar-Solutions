0bda5ebbcec88fe397107c5fc36559d354143b27
Pragti Sharma <sharmapragti07@gmail.com>
2025-02-10 13:42:37 +0530

    Solution to Perform Bitwise Left Shift

--- Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
+++ Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int num, shift_positions, result;
-    scanf("%d%d, &num, &shift_position");
+    scanf("%d %d, &num, &shift_position");
     result = num << shift_positions;
     printf("%d\n", result);
     return 0;
