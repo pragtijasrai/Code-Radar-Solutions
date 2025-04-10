3f0fdb1ff9c6c9c6b4680d440faabd1802afd679
Pragti Sharma <sharmapragti07@gmail.com>
2025-04-10 18:12:57 +0530

    Solution to Reverse Words in a String

--- Reverse Words in a String@@676c039d80e1714600f6764c/code.c
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-// Function to reverse characters from index 'start' to 'end' in the string
-void reverse(char str[], int start, int end) {
+// Function to reverse a word from index start to end in the string
+void reverseWord(char *str, int start, int end) {
     while (start < end) {
         char temp = str[start];
         str[start] = str[end];
@@ -15,28 +16,28 @@ void reverse(char str[], int start, int end) {
 int main() {
     char str[1000];
     
-    // Read the whole line of input
+    // Read a line of input
     fgets(str, sizeof(str), stdin);
 
-    int i = 0, start = 0;
+    int len = strlen(str);
 
-    // Process each word in the string
-    while (str[i] != '\0') {
-        // If space or end of line is found, reverse the word
-        if (str[i] == ' ' || str[i] == '\n' || str[i] == '\0') {
-            reverse(str, start, i - 1);
+    // Remove newline if present
+    if (str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+
+    int start = 0;
+    for (int i = 0; i <= len; i++) {
+        // When a space or end of string is encountered, reverse the word
+        if (str[i] == ' ' || str[i] == '\0') {
+            reverseWord(str, start, i - 1);
             start = i + 1;
         }
-        i++;
     }
 
-    // Handle the last word if the string doesn't end with a newline
-    if (str[i - 1] != '\n' && str[i - 1] != ' ') {
-        reverse(str, start, i - 1);
-    }
-
-    // Print the result
-    printf("%s", str);
+    // Print the modified string
+    printf("%s\n", str);
 
     return 0;
 }
